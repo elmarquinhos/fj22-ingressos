6da11f8bd9c9a4c1abe63e429861e8a06da8a4c1
Antonio Marcos <antonio@marcos.com.br>
2017-09-12 16:57:01 -0300

    Implementado rotina para descontos e rotina que busca informacao de um webservices

--- src/main/resources/META-INF/sql/dados-iniciais.sql
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -2,6 +2,6 @@ insert into ingresso.Sala (id, nome, preco) values (1, "Jujuba", 5.0);
 insert into ingresso.Sala (id, nome, preco) values (2, "HeDoidé", 0.0);
 insert into ingresso.Sala (id, nome, preco) values (3, "NaoEntreAqui", 2.0);
 
-insert into ingresso.Filme (id, nome, duracao, genero, preco) values (1, "Rogue One", 120, "Massa", 10.0);
-insert into ingresso.Filme (id, nome, duracao, genero, preco) values (2, "A Volta dos que ja foram", 90, "Louco", 9.0);
-insert into ingresso.Filme (id, nome, duracao, genero, preco) values (3, "Depois do fim de tudo", 150, "VaoQuebrarTudo", 1000.0);
+insert into ingresso.Filme (id, nome, duracao, genero, preco) values (1, "Rogue One", 7200000000000, "Massa", 10.0);
+insert into ingresso.Filme (id, nome, duracao, genero, preco) values (2, "A Volta dos que ja foram", 5400000000000, "Louco", 9.0);
+insert into ingresso.Filme (id, nome, duracao, genero, preco) values (3, "Depois do fim de tudo", 9000000000000, "VaoQuebrarTudo", 1000.0);
